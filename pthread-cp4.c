3bfb4840c49cd4866e0589c017dca8510692c06f
sameh <sameh.bilto@hotmail.com>
2018-12-15 23:37:32 +0100

    First commit

---
+++ pthread-cp4.c
@@ -0,0 +1,59 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+
+volatile long int a = 0;
+
+void threadFun(void *arg)
+{
+    int i;
+    long int localA =0;
+    
+    for(i = 1; i < 500000; i++)
+    {
+        localA = localA + i;
+    }
+        pthread_mutex_lock(&a);
+        a = a + i;
+        pthread_mutex_unlock(&a);
+}
+
+void threadFun2(void *arg)
+{
+    int i;
+    long int localA =0;
+    
+    for(i = 500000; i < 1000000; i++)
+    {
+        localA = localA + i;
+        
+    }
+        pthread_mutex_lock(&a);
+        a = a + i;
+        pthread_mutex_unlock(&a);
+
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t one, two;
+    int i;
+    a = 0;
+
+    /*
+    pthread_create(&one, NULL, (void*)&threadFun, NULL);
+    pthread_create(&two, NULL, (void*)&threadFun2, NULL);
+
+    pthread_join(one, NULL;
+    pthread_join(two, NULL);
+    */
+
+   for (i = 1; i <= 1000000; i++)
+   {
+       a = a + i; 
+   }
+    printf("%ld\n", a);
+
+}
